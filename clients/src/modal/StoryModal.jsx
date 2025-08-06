daefce528c10b42a3f20061a4dab42ee05b60f3d
inyouxg <dolley03@naver.com>
2025-08-06 16:17:19 +0900

    [Feat] 동화 모아보기 모달 구현

--- clients/src/modal/StoryModal.jsx
+++ clients/src/modal/StoryModal.jsx
@@ -1,6 +1,46 @@
-function StoryModal(){
+import { useState } from 'react'
+import close from '../assets/close.svg'
+import leftArrow from '../assets/leftArrow.svg'
+import rightArrow from '../assets/rightArrow.svg'
+import diary from '../mock/diary.json'
+import story from '../mock/fairyTale.json'
+import "./StoryModal.css"
+
+function StoryModal({onClose}){
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const currentDiary = diary[currentIndex];
+  const day = diary[currentIndex].created_at.slice(0, 10).split("-");
+
+  const prevDiary = () => {
+    if (currentIndex < diary.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+  const nextDiary = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
   return(
-    <></>
+    <div className="modal-container">
+      <div className="modal-wrapper">
+        <img className="close-button" src={close} onClick={onClose}/>
+        <div className="title">
+          <img className="left-arrow" 
+            onClick={prevDiary}
+            src={leftArrow}/>
+            {day[0]}년 {day[1]}월 {day[2]}일
+          <img className='right-arrow' 
+            src={rightArrow}
+            onClick={nextDiary}/>
+        </div>
+        <div className="sketchbook">
+          <img src={story[currentIndex].imageUrl} />
+          <p>{story[currentIndex].text}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 export default StoryModal
